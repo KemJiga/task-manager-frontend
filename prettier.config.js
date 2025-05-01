6bd38ef205de5c8480d3edb72ded834d58836050
Kemjiga <kemerjg@gmail.com>
2025-05-01 01:05:18 -0500

    #3 - add firebase and prettier config and env files

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true, // Add semicolons at the end of statements
+  singleQuote: true, // Use single quotes instead of double quotes
+  trailingComma: "all", // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  printWidth: 80, // Line wrap at 80 characters
+  tabWidth: 2, // Indentation size
+  useTabs: false, // Use spaces instead of tabs
+  bracketSpacing: true, // Add spaces between brackets in object literals
+  arrowParens: "always", // Always include parentheses in arrow functions
+};
