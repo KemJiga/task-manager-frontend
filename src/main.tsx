f439f153a5c7696841561bd2a066fc0ece6f9ddb
Kemjiga <kemerjg@gmail.com>
2025-05-02 17:05:26 -0500

    #14 - add react router implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { AuthProvider } from './context/AuthContext.tsx';
+import App from './App.tsx';
+import 'antd/dist/reset.css';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <BrowserRouter>
+      <StrictMode>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </StrictMode>
+    </BrowserRouter>,
+  );
+} else {
+  console.error('Failed to find the root element');
+}
